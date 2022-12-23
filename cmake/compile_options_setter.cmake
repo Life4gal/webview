66ef1150a7599e660a32b355a80a88ceaf04f783
life4gal <life4gal@gmail.com>
2022-12-23 13:31:30 +0800

    Clang-cl compile options fix.

--- cmake/compile_options_setter.cmake
+++ cmake/compile_options_setter.cmake
@@ -10,14 +10,31 @@ function(set_compile_property target_name)
 		set_target_properties(
 				${target_name}
 				PROPERTIES
-				COMPILE_OPTIONS "/W4;/WX"
+				COMPILE_OPTIONS "/W4;/WX;/D_CRT_SECURE_NO_WARNINGS"
 		)
-	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 		set_target_properties(
 				${target_name}
 				PROPERTIES
 				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
 		)
+	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+		if ("${CMAKE_CXX_SIMULATE_ID}" STREQUAL "MSVC")
+			# clang-cl
+			# todo: It looks like the above command passes -std=c++20 to clang-cl (instead of /std:c++latest)
+			set_target_properties(
+					${target_name}
+					PROPERTIES
+					COMPILE_OPTIONS "/W4;/WX;/D_CRT_SECURE_NO_WARNINGS;/std:c++latest"
+			)
+		else ()
+			# clang
+			set_target_properties(
+					${target_name}
+					PROPERTIES
+					COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
+			)
+		endif ()
 	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
 		set_target_properties(
 				${target_name}
@@ -25,6 +42,6 @@ function(set_compile_property target_name)
 				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
 		)
 	else ()
-		message("Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
+		message(FATAL_ERROR "Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
 	endif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 endfunction(set_compile_property)
