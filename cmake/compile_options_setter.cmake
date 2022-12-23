f524f14707288c25f79fe9459e33687fc37ded5f
life4gal <life4gal@gmail.com>
2022-12-23 13:02:19 +0800

    Add AppleClang check.

--- cmake/compile_options_setter.cmake
+++ cmake/compile_options_setter.cmake
@@ -12,28 +12,12 @@ function(set_compile_property target_name)
 				PROPERTIES
 				COMPILE_OPTIONS "/W4;/WX"
 		)
-	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 		set_target_properties(
 				${target_name}
 				PROPERTIES
 				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
 		)
-	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-		if ("${CMAKE_CXX_SIMULATE_ID}" STREQUAL "MSVC")
-			# clang-cl
-			set_target_properties(
-					${target_name}
-					PROPERTIES
-					COMPILE_OPTIONS "/W4;/WX"
-			)
-		else ()
-			# clang
-			set_target_properties(
-					${target_name}
-					PROPERTIES
-					COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
-			)
-		endif ()
 	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
 		set_target_properties(
 				${target_name}
@@ -41,6 +25,6 @@ function(set_compile_property target_name)
 				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
 		)
 	else ()
-		message(FATAL_ERROR "Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
+		message("Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
 	endif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 endfunction(set_compile_property)
